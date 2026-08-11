93f97ece6114c1fdca3eeb15f53e93b99dd2b514
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-11 19:00:34 -0300

    chore: Adicionando estrutura inicial spring

---
+++ Insight-Auth/src/main/java/com/example/Insight_Auth/InsightAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.Insight_Auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsightAuthApplication.class, args);
+	}
+
+}
---
+++ Insight-Auth/src/test/java/com/example/Insight_Auth/InsightAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Insight_Auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
